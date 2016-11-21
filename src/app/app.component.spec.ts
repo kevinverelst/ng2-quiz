e9ec4128a126ea7240cdbc922d7a4e0a260b7779
Kevin Verelst <kevin.verelst@euri.com>
2016-11-21 21:01:19 +0100

    initial project setup

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,14 @@
+// /**
+//  * Created by kevin on 21/11/2016.
+//  */
+// import { TestBed } from '@angular/core/testing';
+// import { AppComponent } from './app.component';
+// describe('App', () => {
+//     beforeEach(() => {
+//         TestBed.configureTestingModule({ declarations: [AppComponent]});
+//     });
+//     it ('should work', () => {
+//         let fixture = TestBed.createComponent(AppComponent);
+//         expect(fixture.componentInstance instanceof AppComponent).toBe(true, 'should create AppComponent');
+//     });
+// });
---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by kevin on 21/11/2016.
+ */
+import { Component } from '@angular/core';
+import '../../public/css/styles.css';
+@Component({
+    selector: 'my-app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent { }
